d91875455b83767c95430247654aa0b4eb4c7ed0
Natanael Forte <natanaelforte@gmail.com>
2023-03-29 10:19:28 -0300

    tela de cadastro

--- lib/src/homePage.dart
+++ lib/src/homePage.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
+
 import '../widGets/textField.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      child: Column( 
         children: [
           buildTextField('Nome'),
           buildTextField('Email'),
+          InkWell(
+            onTap: () {},
+            child: Ink(
+              
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(123, 60, 28, 134),
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(5)
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical:10 ,
+                  horizontal:42 
+                ),
+                child: Text('Cadastrar', style: TextStyle(
+                fontSize: 25, 
+              ),),
+              )
+            ),
+          ),
         ],
       ),
     );
